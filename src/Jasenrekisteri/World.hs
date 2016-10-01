3de421e6cefe045280744f8c98a5d5883e772552
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-10-01 22:00:28 +0300

    Better world

--- src/Jasenrekisteri/World.hs
+++ src/Jasenrekisteri/World.hs
@@ -6,10 +6,14 @@ module Jasenrekisteri.World (
     -- * Lenses
     worldMembers,
     worldTags,
-    -- * Query
+    -- * Getters
+    worldPersonTags,
+    worldTagPersons,
+    -- * Misc
+    personHasTag,
     ) where
 
--- import Control.Lens
+import Control.Lens
 import Futurice.Prelude
 import Prelude ()
 
@@ -18,27 +22,75 @@ import Data.Vector.Lens (toVectorOf)
 import Jasenrekisteri.Person
 import Jasenrekisteri.Tag
 
+-- | 'World' contains all the data we display.
+--
 data World = World
     { _worldMembers :: !(Vector Person)
     , _worldTags    :: !TagHierarchy
-    -- * Lazy fields, constructed on need
-    -- ,  _worldPersonTags :: Map PersonId Tag
-    --   -- ^ All flags person has, included children tags
-    -- , _worldTagPersons :: Map TagName [Person]
-    --   -- ^ All persons with the tag
+
+    -- Lazy fields, constructed on need:
+    --
+    , _worldPersonTags :: Map PersonId [Tag]
+      -- ^ All flags person has, included children tags
+    , _worldTagPersons :: Map TagName [Person]
+      -- ^ All persons with the tag
     }
 
-makeLenses ''World
+-------------------------------------------------------------------------------
+-- Smart constructor
+-------------------------------------------------------------------------------
 
+-- TODO: remove cycles,  change to return diagnostics too
 mkWorld
     :: (Foldable f, Foldable f')
     => f Person
     -> f' Tag
     -> World
-mkWorld persons tags = World
-    { _worldMembers = toVectorOf folded persons
-    , _worldTags    = tagHierarchyOf folded tags
+mkWorld persons tags = mkWorld'
+    (toVectorOf folded persons)
+    (tagHierarchyOf folded tags)
+
+mkWorld' :: Vector Person -> TagHierarchy -> World
+mkWorld' persons tags = World
+    { _worldMembers = persons
+    , _worldTags    = tags
+    -- TODO:
+    , _worldPersonTags = mempty
+    , _worldTagPersons = mempty
     }
 
+-------------------------------------------------------------------------------
+-- Lenses
+-------------------------------------------------------------------------------
+
+worldMembers :: Lens' World (Vector Person)
+worldMembers = lens _worldMembers $ \world members ->
+    mkWorld' members (world ^. worldTags)
+
+worldTags :: Lens' World TagHierarchy
+worldTags = lens _worldTags $ \world tags ->
+    mkWorld' (world ^. worldMembers) tags
+
+-- |
+--
+-- @
+-- worldPersonTags :: Getter World (Map Personid [Tag])
+-- @
+worldPersonTags :: (Profunctor p, Contravariant f) => Optic' p f World (Map PersonId [Tag])
+worldPersonTags = to _worldPersonTags
+
+-- |
+--
+-- @
+-- worldTagPersons :: Getter World (Map TagName [Person])
+-- @
+worldTagPersons :: (Profunctor p, Contravariant f) => Optic' p f World (Map TagName [Person])
+worldTagPersons = to _worldTagPersons
+
+-------------------------------------------------------------------------------
+-- Qiery
+-------------------------------------------------------------------------------
+
 -- TODO:
--- personHasTag :: World -> Person -> TagName -> Bool
+personHasTag :: World -> Person -> TagName -> Bool
+personHasTag _world _person _tag = False
